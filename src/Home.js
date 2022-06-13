dc8b87605d82d595801672ce57ac0db46ddb5e22
Gustavo <gar.santeliz@gmail.com>
2022-06-12 21:01:23 -0400

    setting up content inside the component

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import { Header } from './components/Header'
-import { Navbar } from './components/Navbar'
-import { Gallery } from './components/Gallery'
 
-// import { CustomersReviews } from './components/CustomersReviews'
-// import { Video } from './components/Video'
-import { Products } from './pages/Products'
 import '../src/styles/home.scss'
 
 export const Home = () => {
@@ -13,24 +8,6 @@ export const Home = () => {
         <>
             <div>
                 <Header />
-            </div>
-            <div>
-                <Navbar />
-            </div>
-            <div className='home-container'>
-                <Products />
-                
-                <div className='gallery-div'>
-                    <h1 className='gallery-title'>Galeria</h1>
-                    <Gallery />
-                </div>
-                {/* <div>
-                    <Video />
-                </div> */}
-                {/* <div>
-                    <CustomersReviews />
-                </div> */}
-
             </div>
         </>
     )
