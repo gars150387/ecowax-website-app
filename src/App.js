3d9444a4aa897eb5ae3f59119f8dc1d9d0b099c3
Gustavo <gar.santeliz@gmail.com>
2022-06-12 21:00:59 -0400

    containing routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Gallery } from "./pages/Gallery";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./Home";
+import { Products } from "./pages/Products";
+import { Video } from "./pages/Video";
 
 export const App = () => {
   return (
-    <div></div>
-  )
-}
+    <Router>
+       <Home />
+       <Navbar />
+       <Products />
+       <Gallery />
+       <Video />
+    </Router>
+  );
+};
