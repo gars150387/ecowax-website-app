f8b84387e5046e50978fcec2d6d6e77cd70c8657
Gustavo <gar.santeliz@gmail.com>
2022-06-13 20:09:24 -0400

    last touches before to start to work styling for smaller screens

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Gallery } from "./pages/Gallery";
-import { Navbar } from "./components/Navbar";
 import { Home } from "./Home";
-import { Products } from "./pages/Products";
-import { Video } from "./pages/Video";
 
 export const App = () => {
   return (
     <Router>
-       <Home />
-       <Navbar />
-       <Products />
-       <Gallery />
-       <Video />
+       <Home title='home' />
+       
     </Router>
   );
 };
--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,29 @@
-import React from 'react'
-import { Header } from './components/Header'
+import React from "react";
+import { Header } from "./components/Header";
+import { Products } from "./pages/Products";
+import { Gallery } from "./pages/Gallery";
+// import { Video } from "./pages/Video";
+import { Navbar } from "./components/Navbar";
 
-import '../src/styles/home.scss'
+import "../src/styles/home.scss";
+import { ScrollUp } from "./hooks/ScrollUp";
+import { Footer } from "./components/Footer";
 
 export const Home = () => {
-    return (
-        <>
-            <div>
-                <Header />
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Navbar />
+        <Products title="products" />
+        <Gallery title="gallery" />
+        {/* <Video title="video" /> */}        
+        <ScrollUp />
+
+        <Footer />
+      </div>
+    </>
+  );
+};
