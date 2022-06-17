a9a47dcb5e38991fb030f8fafb67a9c795ece520
Gustavo <gar.santeliz@gmail.com>
2022-06-16 20:51:01 -0400

    video component added

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Header } from "./components/Header";
 import { Products } from "./pages/Products";
 import { Gallery } from "./pages/Gallery";
-// import { Video } from "./pages/Video";
+import { Video } from "./pages/Video";
 import { Navbar } from "./components/Navbar";
 
 import "../src/styles/home.scss";
@@ -19,7 +19,7 @@ export const Home = () => {
         <Navbar />
         <Products title="products" />
         <Gallery title="gallery" />
-        {/* <Video title="video" /> */}        
+        <Video title="video" />        
         <ScrollUp />
 
         <Footer />
