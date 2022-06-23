5f9ffeb633a3afe7946d982542544340a82077bc
Gustavo <gar.santeliz@gmail.com>
2022-06-22 22:19:19 -0400

    rewriting ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import '../src/styles/index.scss'
-import { App } from './App';
+import ReactDOM from 'react-dom';
+import "../src/styles/index.scss";
+import { App } from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+    <App />,
+  document.getElementById("root")
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
